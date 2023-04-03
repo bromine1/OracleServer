7121e392a4be7ab55a35939e680d175eaa64d2c8
bromine1 <bromine1>
2023-04-02 20:46:16 -0400

    fix test for MC

--- hosts/MC.nix
+++ hosts/MC.nix
@@ -1,13 +1,25 @@
 { inputs, ... }:
 {
   imports = [ inputs.ModdedMC.module ];
-
-  services.modded-minercraft-servers = {
+  services.modded-minecraft-servers = {
     eula = true;
 
     #Servers are stored in /var/name
-    temp = {
-      enable = true;
+    instances = {
+      e2es = {
+        enable = true;
+
+        rsyncSSHKeys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDGldrYw25J0K2LyZNhTakj66yYrUfrmK34WiyHa/91r ryan@Galaxia"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPLMtBjXvadChqa2pZIvJ6eHrkcYD87/skfl3Kjwg6dO ryan@nixos"
+        ];
+
+        serverConfig = {
+          # Port must be unique
+          server-port = 25566;
+          motd = "Welcome to Enigmatica 2: Expert Skyblock";
+        };
+      };
     };
   };
 
